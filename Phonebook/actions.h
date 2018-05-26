e6cd6db84573d8d3ce565f5151d8f83f24a66920
Skytlz <Skytlzdev@gmail.com>
2018-05-26 17:13:18 -0500

    Alphabet function added. Determines left or right of tree.

--- Phonebook/actions.h
+++ Phonebook/actions.h
@@ -21,7 +21,7 @@ struct entry {
 	//string occupation;
 };
 
-struct node{
+struct node {
 	entry input;
 	node* left;
 	node* right
@@ -32,7 +32,7 @@ node* bhome = new node;
 node* home = new node;
 node* newNode = new node;
 
-bool alphabet(); //Determine left or right.
+bool alphabet(string); //Determine left or right.
 void Entry(); //input to node;
 void search(); //Search for name
 void printone(node*); //Prints one node.
@@ -40,3 +40,35 @@ void printone(node*); //Prints entire tree.
 void deleteOne(string); //Deletes a node from the tree.
 void edit(string); //Edits one node. 
 
+bool alphabet(string alpha) {
+	unsigned long alphaLength = alpha.length();
+	unsigned long firstNameLength = current->input.firstName.length();
+
+	bool side = false;
+
+	if (alphaLength >= firstNameLength) {
+		for (int i = 0; i < current->input.firstName.length(); i++) {
+			if (alpha.at(i) < current->input.firstName.at(i)) {
+				side = false;
+				break;
+			}
+			else if (alpha.at(i) > current->input.firstName.at(i)) {
+				side = true;
+				break;
+			}
+		}
+	}
+	else if (alphaLength < firstNameLength) {
+		for (int i = 0; i < alphaLength - 1; i++) {
+			if (alpha.at(i) > current->input.firstName.at(i)) {
+				side = true;
+				break;
+			}
+			else if (alpha.at(i) < current->input.firstName.at(i)) {
+				side = false;
+				break;
+			}
+		}
+	}
+	return side;
+}
