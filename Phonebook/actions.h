782cb4e90a0311a6226f0d85474a8741e822a1f4
Jack Gurley <skytlzdev@gmail.com>
2018-10-15 14:10:31 -0500

    Revert "PrintAll() works, small error, working on fix."
    This reverts commit 4a7b8d29448396d448c8e50be0a2d8ca21b09ec9.

--- Phonebook/actions.h
+++ Phonebook/actions.h
@@ -26,14 +26,12 @@ struct node {
 	entry input;
 	node* left;
     node* right;
-	node() : left(nullptr), right(nullptr) {}
 };
 node* current;
 node* bcurrent;
 node* bhome = new node;
 node* home = new node;
 node* newNode = new node;
-
 
 bool alphabet(string); //Determine left or right.
 void Entry(); //input to node;
@@ -159,13 +157,13 @@ void search(string name) {
 
 }
 
-void printAll(struct node* start) {
-	if (start == NULL) return;
+void printAll(struct node* node) {
+	if (node == NULL) return;
 	
 
-	printAll(start->left); 
-	cout << start->input.firstName << endl;
-	printAll(start->right);
+	printAll(node->left); //Unhandled exception thrown: read access violation. **start** was 0xCDCDCDCD. occurred
+	cout << node->input.firstName << endl;
+	printAll(node->right);
 
 }
 
